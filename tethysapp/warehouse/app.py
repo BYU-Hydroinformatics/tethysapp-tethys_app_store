da9a7f4e5a4e8532679c4e36fa44563f55f53340
Rohit Khattar <rohitkh@byu,edu>
2019-04-05 13:45:23 -0600

    Init commit

---
+++ tethysapp/warehouse/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class Warehouse(TethysAppBase):
+    """
+    Tethys app class for Warehouse.
+    """
+
+    name = 'Warehouse'
+    index = 'warehouse:home'
+    icon = 'warehouse/images/icon.gif'
+    package = 'warehouse'
+    root_url = 'warehouse'
+    color = '#c0392b'
+    description = 'Place a brief description of your app here.'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='warehouse',
+                controller='warehouse.controllers.home'
+            ),
+        )
+
+        return url_maps
