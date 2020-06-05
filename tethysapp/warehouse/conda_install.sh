457ea8fb1a5a602916d082be8e33913c27491215
Rohit Khattar <rohitkh@byu,edu>
2020-06-05 11:55:36 -0600

    Minor changes to conda install command

--- tethysapp/warehouse/conda_install.sh
+++ tethysapp/warehouse/conda_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Running Conda Install"
-conda install -y -S -q -c $2 $1
+conda install -y --freeze-installed -q -c $2 -c tethysplatform -c conda-forge $1
 echo "Conda Install Complete"
