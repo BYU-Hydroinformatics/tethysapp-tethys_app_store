5ac1d27d0f0111fe03d1c1cd347226c073cb0f8a
romer8 <gio.busrom@gmail.com>
2023-01-04 21:38:11 -0700

    possible fix 2 for the lost on signal

--- tethysapp/app_store/installation_handlers.py
+++ tethysapp/app_store/installation_handlers.py
@@ -84,9 +84,10 @@ def restart_server(data, channel_layer, app_workspace, run_collect_all=True):
 
         try:
             # command = 'supervisorctl restart all'
-            subprocess.run(['sudo','-h'], check=True)
+            # subprocess.run(['sudo','-h'], check=True)
             sudoPassword = app.get_custom_setting('sudo_server_pass')
-            subprocess.call(["sudo", "-S", "supervisorctl", "restart", "all"])
+            subprocess.Popen(['sudo', '-S', 'supervisorctl', 'restart','all'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate(input=f'{sudoPassword}\n')
+            # subprocess.run(["sudo", "-S", "supervisorctl", "restart", "all"])
             # os.system('echo %s|sudo -S %s' % (sudoPassword, command))
         except Exception as e:
             logger.error(e)
