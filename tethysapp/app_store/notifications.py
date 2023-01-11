6dbabe6a99773908d8cc59c74fccd98f011fe5f1
romer8 <gio.busrom@gmail.com>
2023-01-11 13:52:13 -0700

    need to import process_branch function for the thredds process to work

--- tethysapp/app_store/notifications.py
+++ tethysapp/app_store/notifications.py
@@ -5,7 +5,7 @@ from .git_install_handlers import get_log_file # noqa: F401
 from .submission_handlers import pull_git_repo # noqa: F401
 from .update_handlers import update_app # noqa: F401
 from .resource_helpers import clear_cache # noqa: F401
-
+from .submission_handlers import process_branch # noqa: F401
 # called with threading.Thread
 from .begin_install import begin_install  # noqa: F401
 from tethys_sdk.routing import consumer
